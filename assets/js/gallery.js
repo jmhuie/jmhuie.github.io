723e78edd50909addc05281c373d1cd851e656bf
jmhuie <52302862+jmhuie@users.noreply.github.com>
2025-11-03 12:08:38 -0800

    2025 revamp pt1

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -6,54 +6,51 @@ const close = document.querySelector(".close");
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
 
+let imageIndex = 0; // Keep track of current image globally
+
+// Open modal when image clicked
 images.forEach((image, index) => {
   image.addEventListener("click", () => {
-    modalImg.src = image.src;
-    modalTxt.innerHTML = image.alt;
+    imageIndex = index;
+    showImage(imageIndex);
     modal.classList.add("appear");
-
-    let imageIndex = index;
-    let next = imageIndex++;
-    let prev = imageIndex--;
-
-    window.addEventListener("keyup", (e) => {
-      /*if (next >= images.length) {
-              next = 0;
-            } else if (prev < 0) {
-              prev = images.length - 1;
-            }*/
-
-      if (e.keyCode === 37) {
-        modalImg.src = images[prev].src;
-        modalTxt.innerHTML = images[prev].alt;
-        prev--;
-        next = prev + 2;
-      } else if (e.keyCode === 39) {
-        modalImg.src = images[next].src;
-        modalTxt.innerHTML = images[next].alt;
-        next++;
-        prev = next - 2;
-      } else if (e.keyCode === 27) {
-        modal.classList.remove("appear");
-      }
-    });
-
-    prevBtn.addEventListener("click", () => {
-      modalImg.src = images[prev].src;
-      modalTxt.innerHTML = images[prev].alt;
-      prev--;
-      next = prev + 2;
-    });
-
-    nextBtn.addEventListener("click", () => {
-      modalImg.src = images[next].src;
-      modalTxt.innerHTML = images[next].alt;
-      next++;
-      prev = next - 2;
-    });
-
-    close.addEventListener("click", () => {
-      modal.classList.remove("appear");
-    });
   });
+});
+
+function showImage(index) {
+  if (index >= images.length) imageIndex = 0;
+  if (index < 0) imageIndex = images.length - 1;
+  modalImg.src = images[imageIndex].src;
+  modalTxt.innerHTML = images[imageIndex].alt;
+}
+
+// Navigation buttons
+nextBtn.addEventListener("click", () => {
+  imageIndex++;
+  showImage(imageIndex);
+});
+
+prevBtn.addEventListener("click", () => {
+  imageIndex--;
+  showImage(imageIndex);
+});
+
+// Keyboard navigation
+window.addEventListener("keyup", (e) => {
+  if (!modal.classList.contains("appear")) return;
+
+  if (e.key === "ArrowRight") {
+    imageIndex++;
+    showImage(imageIndex);
+  } else if (e.key === "ArrowLeft") {
+    imageIndex--;
+    showImage(imageIndex);
+  } else if (e.key === "Escape") {
+    modal.classList.remove("appear");
+  }
+});
+
+// Close modal
+close.addEventListener("click", () => {
+  modal.classList.remove("appear");
 });
